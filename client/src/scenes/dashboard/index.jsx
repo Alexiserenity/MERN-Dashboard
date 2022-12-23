4b049bc9307e212bf4f8dc2a62ab39448ab45f02
adrien <adrien.ceza@gmail.com>
2022-12-23 21:04:06 +0100

    API meteo

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import FlexBetween from "components/FlexBetween";
 import Header from "components/Header";
 import {
@@ -7,6 +7,7 @@ import {
   PointOfSale,
   PersonAdd,
   Traffic,
+  Search,
 } from "@mui/icons-material";
 import {
   Box,
@@ -14,12 +15,18 @@ import {
   Typography,
   useTheme,
   useMediaQuery,
+  Toolbar,
 } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import BreakdownChart from "components/BreakdownChart";
 import OverviewChart from "components/OverviewChart";
 import { useGetDashboardQuery } from "state/api";
 import StatBox from "components/StatBox";
+
+const api = {
+  key: "0638574d291b3e1090515ce7939cd186",
+  base: " http://api.openweathermap.org/data/2.5/"
+}
 
 const Dashboard = () => {
   const theme = useTheme();
@@ -56,11 +63,85 @@ const Dashboard = () => {
       renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
     },
   ];
+ 
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+ 
+  
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+ 
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+ 
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+ 
+    return `${day} ${date} ${month} ${year}`
+  }
+ 
+
+
 
   return (
     <Box m="1.5rem 2.5rem">
       <FlexBetween>
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+
+        <Box>
+          <Toolbar>
+          
+          
+ 
+ 
+ 
+   <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+     <main>
+       <div className="search-box">
+         <input 
+           type="text"
+           className="search-bar"
+           placeholder="meteo location..."
+           onChange={e => setQuery(e.target.value)}
+           value={query}
+           onKeyPress={search}
+         />
+         
+       </div>
+       {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="temp"> {Math.round(weather.main.temp)}°c </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
+        </div>
+       ) : ('')}
+     </main>
+   </div>
+ 
+
+
+
+ 
+            
+          
+          </Toolbar>
+        </Box>
+
+
 
         <Box>
           <Button
